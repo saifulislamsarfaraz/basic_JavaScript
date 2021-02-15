935ca72e4d0a83f87b0e92e64300540d5651a4ba
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-02-15 11:09:43 +0600

    ok

--- hellow-world.js
+++ hellow-world.js
@@ -1,1 +1,16 @@
-console.log("Hello ok kok world okoko thanks  thanks")
+let js = 'decent';
+if (js === 'decent') alert('wow nice its work');
+let first_name = 'saiful';
+console.log(first_name);
+console.log(first_name);
+console.log(first_name);
+console.log(first_name);
+console.log(first_name);
+
+let javascriptIsFun = true;
+console.log(typeof true);
+console.log
+
+//console.log(5 + 6 + 5 + 5 + 56);
+
+
