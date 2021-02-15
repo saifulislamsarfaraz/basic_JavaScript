f92fbf020887a9e178523c13954e34207f31da48
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-02-15 16:36:13 +0600

    comparison operatiors

--- hellow-world.js
+++ hellow-world.js
@@ -9,8 +9,103 @@ console.log(first_name);
 
 let javascriptIsFun = true;
 console.log(typeof true);
-console.log
+console.log(typeof 65);
+console.log(typeof "this");
+year = 1191
+console.log(typeof year);
+console.log(typeof null);
+const birthYear = 1991;
+
 
 //console.log(5 + 6 + 5 + 5 + 56);
+var pneumonoultramicrescompicsilicovoclanoconiosis = 'programmer';
+pneumonoultramicrescompicsilicovoclanoconiosis = 'teacher';
+console.log(pneumonoultramicrescompicsilicovoclanoconiosis);
+lastname = 'so you will understand little bit letter';
+console.log(lastname);
+var rohimName = 'rohim';
+korimName = 'korim';
+console.log(rohimName, korimName);
+const youKnow = 89 - 32;
+const firstName = 'saif';
+const lastName = 'islam';
+console.log(firstName + ' ' + lastname);
+
+
+
+//comparison operatiors
+//console.log ()
+
+var marksWeights = 78;
+var marksHeight = 1.69;
+var marksBMI = (marksWeights / (marksHeight ** 2));
+console.log(marksBMI);
+var johnsWeights = 92;
+var johnsHeight = 1.95;
+var johnsBMI = (johnsWeights / (johnsHeight ** 2));
+console.log(johnsBMI);
+console.log(marksBMI > johnsBMI);
+
+
+var marksWeights = 95;
+var marksHeight = 1.88;
+var marksBMI = (95 / (1.88 * 1.88));
+console.log(marksBMI);
+var johnsWeights = 85;
+var johnsHeight = 1.76;
+var johnsBMI = (95 / (1.76 * 1.76));
+console.log(johnsBMI);
+console.log(marksBMI > johnsBMI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
