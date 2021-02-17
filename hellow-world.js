d1f76fd5d891389ac3c651b268c784c28ec71579
saifulislamsarfaraz <62655613+saifulislamsarfaraz@users.noreply.github.com>
2021-02-18 00:21:32 +0600

    add

--- hellow-world.js
+++ hellow-world.js
@@ -34,7 +34,7 @@ console.log(firstName + ' ' + lastname);
 
 //comparison operatiors
 //console.log ()
-
+*/
 var marksWeights = 78;
 var marksHeight = 1.69;
 var marksBMI = (marksWeights / (marksHeight ** 2));
@@ -55,7 +55,17 @@ var johnsHeight = 1.76;
 var johnsBMI = (85 / (1.76 * 1.76));
 console.log(johnsBMI);
 console.log(marksBMI > johnsBMI);
-*/
+if (marksBMI > johnsBMI) {
+  console.log(`Marks BMI is higher than Johns`);
+} else {
+  console.log(`Johns BMI is higher than marks!`);
+}
+
+if (marksBMI > johnsBMI) {
+  console.log(`Makrs BMI (${marksBMI}) values is higher than Johns (${johnsBMI})`);
+} else {
+  console.log(`Johns BMI (${johnsBMI}) values is higher than Marks (${marksBMI})`);
+}
 const firstName = 'saiful';
 const lastName = 'islam';
 const ocupation = 'web developer';
@@ -67,59 +77,52 @@ console.log(saif);
 //upgrade 
 const saifNew = `I'am ${firstName} , a ${currentYear - birthYear} year old ${ocupation}!`;
 console.log(saifNew);
-const age = 19;
-const isOldEnough = age >= 18;
+const age = 15;
 
-if (isOldEnough) {
+if (age >= 18) {
   console.log('saiful can start driving license 🚗🚗🚓🚓🚓🚓🚓🚓🚓🚑🚑🚑🚑🚒🚒🚑🚗🚗🚗🚗🚗🚗🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`saiful is too young. wait another ${yearsLeft} years :)`);
 }
 
+//type conversion 
+const inputYear = '1991';
+console.log(Number(inputYear) + 18, inputYear);
+console.log(inputYear + 18);
+//type coercion
+console.log("I'am " + 23 + ' years old.');
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+// 5 falst values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('saiful'));
+console.log(Boolean(''));
+console.log(Boolean({}));
 
+const ages = '18';
+if (ages === 18) console.log('You just became an adult :D(strict)')
+if (ages == 18) console.log('You just became an adult :D(loose)')
 
+const favourite = Number(prompt("what's your favourite number"));
+console.log(favourite);
 
+if (favourite === 23) {
+  console.log('cool! 23 is an amazing number!');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const aTeam = (96 + 108 + 89) / 3;
+const bTeam = (88 + 91 + 110) / 3;
+if (aTeam && bTeam >= 100) {
+  console.log('draw both team');
+} else if (aTeam > 100 && bTeam > 100) {
+  if (aTeam > bTeam)
+    console.log('win team aTeam');
+} else if (bTeam > 100 && aTeam > 100) {
+  if (bTeam > aTeam)
+    console.log('win team bTeam');
+} else if (aTeam === bTeam) {
+  console.log('draw team both');
+} 
